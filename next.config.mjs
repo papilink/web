4905801a398b699ce8b1ad00c42aaae13a76f07c
papiweb <lulaweb2025@gmail.com>
2025-05-05 01:46:29 +0000

    Configuración para despliegue en GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  output: 'export',
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
 }
 
 export default nextConfig
